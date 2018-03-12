cd5d61be3695d6de1b63cc348ebfd1074b2c6931
David Yonwook Chung <yon_gospel@hotmail.com>
2018-03-12 16:17:06 -0700

    [db] student and cohort table successfully created

--- server/database/setup.js
+++ server/database/setup.js
@@ -7,6 +7,7 @@ import {
 
 const setup = async () => {
   // await dropDB();
+  await createCohortTable();
   await createStudentTable();
   process.exit();
 };
--- server/lib/PSQL/index.js
+++ server/lib/PSQL/index.js
@@ -22,26 +22,40 @@ export const dropDB = async () => {
   }
 }
 
-export default createStudentTable = () => {
-  `
-  CREATE TABLE IF NOT EXISTS studentTable
-  (
-    id SERIAL,
-    fullname VARCHAR(50) NOT NULL,
-    username VARCHAR(20) NOT NULL,
-    cohort_id INT NOT NULL,
-    CONSTRAINT fk_studentTable_cohort_id
-      FOREIGN KEY(cohort_id) REFERENCES cohortTable(id)
-  )
-  `
+export const createStudentTable = async () => {
+  try {
+    await db.queryAsync(
+      `
+      CREATE TABLE IF NOT EXISTS studentTable
+      (
+        id integer PRIMARY KEY,
+        fullname VARCHAR(50) NOT NULL,
+        username VARCHAR(20) NOT NULL,
+        cohort_id INT NOT NULL,
+        CONSTRAINT fk_studentTable_cohort_id
+          FOREIGN KEY(cohort_id) REFERENCES cohorttable(id)
+      )
+      `
+    )
+    console.log('Successfully created student table');
+  } catch (err) {
+    console.log('Could not create student table:', err);
+  }
 }
 
-export default createCohortTable = () => {
-  `
-  CREATE TABLE IF NOT EXISTS cohortTable
-  (
-    id SERIAL,
-    cohortname VARCHAR(10) NOT NULL
-  )
-  `
+export const createCohortTable = async () => {
+  try {
+    await db.queryAsync(
+      `
+      CREATE TABLE IF NOT EXISTS cohortTable
+      (
+        id integer PRIMARY KEY,
+        cohortname VARCHAR(10) NOT NULL
+      )
+      `
+    )
+    console.log('Successfully created cohort table');
+  } catch (err) {
+    console.log('Could not create cohort table:', err)
+  }
 }
