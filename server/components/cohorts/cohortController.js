c9b20791627f0b57e3ac429dabe8f8ecc8f1767f
David Yonwook Chung <yon_gospel@hotmail.com>
2018-04-17 17:22:54 -0700

    [pull]

--- server/components/cohorts/cohortController.js
+++ server/components/cohorts/cohortController.js
@@ -6,7 +6,6 @@ import {
 
 export const createCohort = async (req, res) => {
   try {
-    console.log("what i want", req.body);
     const cohort = await createCohortQuery(req.body);
     res.status(200).send(req.body.cohortname);
   } catch (err) {
